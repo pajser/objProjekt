79a5630b9df72b6a9471b0a2f919d68201c44024
ottosinger <otto.singer@fer.hr>
2017-01-27 18:52:28 +0100

    dodane klase za zahtjev

---
+++ WindowsFormsApplication1/ZahtjevPredlozak.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class ZahtjevPredlozak
+    {
+        public int idKorisnickiRacun;
+        public int idTima;
+        public bool iniciraKorisnik;
+        public string poruka;
+        public ZahtjevPredlozak(int idKR, int idT, bool ini, string por)
+        {
+            this.idKorisnickiRacun = idKR;
+            this.idTima = idT;
+            this.iniciraKorisnik = ini;
+            this.poruka = por;
+        }
+
+    }
+}
