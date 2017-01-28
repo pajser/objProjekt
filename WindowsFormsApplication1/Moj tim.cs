1c5d35b15517390f7e9ba739bce8af72e8ebb4fe
pajser <dude_whazzup@hotmail.com>
2017-01-28 13:36:59 +0100

    test finalni

--- WindowsFormsApplication1/Moj tim.cs
+++ WindowsFormsApplication1/Moj tim.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//neka linija
 namespace WindowsFormsApplication1
 {
     public partial class Moj_tim : Form
