cc114a2af37eea2aff3a1321fbe82a98baa656ff
ottosinger <otto.singer@fer.hr>
2017-01-29 15:11:10 +0100

    ispravljeno dodavanje korisnika - provjere polja

--- WindowsFormsApplication1/Novi korisnik.cs
+++ WindowsFormsApplication1/Novi korisnik.cs
@@ -22,8 +22,28 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             DataTable dt = new DataTable();
-            bool uspjesnost = KorisnickiRacunTablica.DodajKorisnika(this.textBox2.Text,this.textBox3.Text, this.textBox1.Text, this.textBox4.Text);
-                this.Close();
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "")
+            {
+                bool uspjesnost = KorisnickiRacunTablica.DodajKorisnika(this.textBox2.Text, this.textBox3.Text, this.textBox1.Text, this.textBox4.Text);
+                if (uspjesnost)
+                {
+                    System.Windows.MessageBox.Show("Korisnik uspješno dodan!");
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Došlo je do problema prilikom dodavanja korisnika!");
+                }
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Nisu popunjena sva polja!");
+            }
+
+            
         }
 
         //private static string connStr = ConfigurationManager.ConnectionStrings["WindowsFormsApplication1.Properties.Settings.bazaOBJConnectionString"].ConnectionString;
